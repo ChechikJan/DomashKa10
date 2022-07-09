6d33f2d36dc61de8dfb374198a7829c2ae88c730
Danila Kalay <ChechikLud@gmail.com>
2022-07-09 16:22:48 +0300

    first commit

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,4 +1,65 @@
 package org.example;
 
-public class Padio {
+
+
+public class Radio {
+    private int radioStationNumber;
+    private int currentVolume;
+
+    public int getRadioStationNumber() {
+        return radioStationNumber;
+    }
+
+    public void setRadioStationNumber(int newStationNumber) {
+
+        if (newStationNumber > 9) {
+            return;
+        }
+        radioStationNumber = newStationNumber;
+
+    }
+
+    public int nextStation() {
+        radioStationNumber = radioStationNumber + 1;
+        if (radioStationNumber > 9) {
+            return 0;
+        }
+        return radioStationNumber;
+    }
+
+    public int prevStation() {
+        radioStationNumber = radioStationNumber - 1;
+        if (radioStationNumber < 0) {
+            return 9;
+        }
+        return radioStationNumber;
+
+
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+
+        if (newCurrentVolume > 10) {
+            newCurrentVolume = 10;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public int increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        return currentVolume;
+    }
+
+    public int decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        return currentVolume;
+    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,82 @@
+import org.example.Radio;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RadioTest {
+    Radio radio = new Radio();
+    @Test
+    public void MoreThanTheMaximumRadioStationNumberTest(){
+        radio.setRadioStationNumber(10);
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void nextStationOverMaxTest() {
+        radio.setRadioStationNumber(9);
+        int expected = 0;
+        int actual = radio.nextStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void nextStationTest() {
+        radio.setRadioStationNumber(7);
+        int expected = 8;
+        int actual = radio.nextStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void prevStationTest() {
+        radio.setRadioStationNumber(5);
+        int expected = 4;
+        int actual = radio.prevStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void prevStationLessMinTest() {
+        radio.setRadioStationNumber(0);
+        int expected = 9;
+        int actual = radio.prevStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setCurrentVolumeTest(){
+        radio.setCurrentVolume(14);
+        int expected = 10;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void increaseVolumeTest(){
+        radio.setCurrentVolume(6);
+        int expected = 7;
+        int actual = radio.increaseVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void increaseVolumeMaxTest(){
+        radio.setCurrentVolume(10);
+        int expected = 10;
+        int actual = radio.increaseVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseVolumeMinTest(){
+        radio.setCurrentVolume(0);
+        int expected = 0;
+        int actual = radio.decreaseVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseVolumeTest(){
+        radio.setCurrentVolume(4);
+        int expected = 3;
+        int actual = radio.decreaseVolume();
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
